f3327f3590f4c9f4e626f5da1351664eb800c76d
khintk <kkyaw@macalester.edu>
2017-02-13 21:37:13 -0600

    Test Commit

--- src/pages/home/home.ts
+++ src/pages/home/home.ts
@@ -4,6 +4,8 @@ import { NavController } from 'ionic-angular';
 
 import { WeatherService } from '../../providers/weather-service';
 
+import { ActionSheetController } from 'ionic-angular';
+
 @Component({
   selector: 'page-home',
   templateUrl: 'home.html',
@@ -13,15 +15,42 @@ export class HomePage {
 
   public weather: any;
 
-  constructor(public navCtrl: NavController, public weatherService: WeatherService) {
+  constructor(public navCtrl: NavController, public weatherService: WeatherService, public actionSheetCtrl: ActionSheetController) {
     this.loadWeather();
   }
 
-  loadWeather(){
+  loadWeather() {
     this.weatherService.load()
       .then(data => {
         this.weather = data;
       })
   }
 
+  presentActionSheet() {
+    let actionSheet = this.actionSheetCtrl.create({
+      title: 'Modify your album',
+      buttons: [
+        {
+          text: 'Destructive',
+          role: 'destructive',
+          handler: () => {
+            console.log('Destructive clicked');
+          }
+        }, {
+          text: 'Archive',
+          handler: () => {
+            console.log('Archive clicked');
+          }
+        }, {
+          text: 'Cancel',
+          role: 'cancel',
+          handler: () => {
+            console.log('Cancel clicked');
+          }
+        }
+      ]
+    });
+    actionSheet.present();
+
+  }
 }
