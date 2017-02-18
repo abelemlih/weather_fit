c72c30b90043f0c714a7eb9be9fd7161e6d11b53
Trung Nguyen <tnguyen9@macalester.edu>
2017-02-17 20:43:32 -0600

    Added geolocation recognition and corresponding API call.

--- src/providers/weather-service.ts
+++ src/providers/weather-service.ts
@@ -2,19 +2,39 @@ import { Injectable } from '@angular/core';
 import { Http } from '@angular/http';
 import 'rxjs/add/operator/map';
 
+import { Geolocation } from 'ionic-native';
+
 /*
   Generated class for the WeatherService provider.
 
   See https://angular.io/docs/ts/latest/guide/dependency-injection.html
   for more info on providers and Angular 2 DI.
 */
+
 @Injectable()
 export class WeatherService {
 
   data: any;
 
-  constructor(public http: Http) {
-    console.log('Hello WeatherService Provider');
+  constructor(public http: Http) {}
+
+  getLocation(){
+    return new Promise((resolve, reject) => {
+      Geolocation.getCurrentPosition()
+        .then((resp) => {
+            let url = "http://api.openweathermap.org/data/2.5/weather?";
+            let lat = Math.round(resp.coords.latitude);
+            let long = Math.round(resp.coords.longitude);
+            url += "lat=" + lat + "&lon=" + long;
+            url += "&APPID=6a24316a673761513e82c0ee0315bdea";
+            resolve(url);
+          }
+        )
+        .catch((error) => {
+          console.log("Error getting location.");
+          reject(error);
+        })
+    })
   }
 
   load() {
@@ -25,12 +45,14 @@ export class WeatherService {
 
     // don't have the data yet
     return new Promise(resolve => {
-      this.http.get("http://api.openweathermap.org/data/2.5/weather?id=5045360&APPID=6a24316a673761513e82c0ee0315bdea")
-        .map(res => res.json())
-        .subscribe(data => {
-          this.data = data;
-          resolve(this.data);
-        })
+      this.getLocation().then((url: string) => {
+        this.http.get(url)
+          .map(res => res.json())
+          .subscribe(data => {
+            this.data = data;
+            resolve(this.data);
+          })
+      })
     })
   }
 }
