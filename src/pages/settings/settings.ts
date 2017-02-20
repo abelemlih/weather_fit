4f25e4d973eb1487d0110cdae1e8df1ebdc5167c
Ayoub <abelemli@macalester.edu>
2017-02-19 22:27:13 -0600

    Link main page to settings page

--- src/pages/settings/settings.ts
+++ src/pages/settings/settings.ts
@@ -2,21 +2,21 @@ import { Component } from '@angular/core';
 import { NavController, NavParams } from 'ionic-angular';
 
 /*
-  Generated class for the MyPage page.
+  Generated class for the Settings page.
 
   See http://ionicframework.com/docs/v2/components/#navigation for more info on
   Ionic pages and navigation.
 */
 @Component({
-  selector: 'page-my-page',
-  templateUrl: 'my-page.html'
+  selector: 'page-settings',
+  templateUrl: 'settings.html'
 })
-export class MyPagePage {
+export class SettingsPage {
 
   constructor(public navCtrl: NavController, public navParams: NavParams) {}
 
   ionViewDidLoad() {
-    console.log('ionViewDidLoad MyPagePage');
+    console.log('ionViewDidLoad SettingsPage');
   }
 
 }
