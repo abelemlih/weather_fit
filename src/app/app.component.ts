8b1ff7b4e8e0acf50581d4d5dbb663fe25adfa66
Trung Nguyen <tnguyen9@macalester.edu>
2017-03-02 00:42:09 -0600

    Moved menu to app.html and updated HomePage to work with it

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,11 @@
-import { Component } from '@angular/core';
-import { Platform } from 'ionic-angular';
+import {Component, ViewChild} from '@angular/core';
+import {Platform, Nav} from 'ionic-angular';
 import { StatusBar, Splashscreen } from 'ionic-native';
 
 import { HomePage } from '../pages/home/home';
 
 import { SettingsService } from '../providers/settings-service';
+import { SettingsPage } from "../pages/settings/settings";
 
 
 @Component({
@@ -12,15 +13,34 @@ import { SettingsService } from '../providers/settings-service';
   providers: [SettingsService]
 })
 export class MyApp {
+  @ViewChild(Nav) nav: Nav;
+
   rootPage = HomePage;
 
-  constructor(platform: Platform, private settingsService: SettingsService) {
-    platform.ready().then(() => {
+  pages: Object;
+
+  constructor(public platform: Platform, private settingsService: SettingsService) {
+    this.initializeApp();
+
+    this.pages = {
+      "Home": HomePage,
+      "Settings": SettingsPage
+    }
+  }
+
+  initializeApp() {
+    this.platform.ready().then(() => {
       // Okay, so the platform is ready and our plugins are available.
       // Here you can do any higher level native things you might need.
-
       StatusBar.styleDefault();
       Splashscreen.hide();
     });
+  }
+
+  openPage(page) {
+    // Reset the content nav to have just this page
+    // we wouldn't want the back button to show in this scenario
+    this.nav.setRoot(page.component)
+      .catch( (error) => console.log("Failed to load " + page.title));
   }
 }
