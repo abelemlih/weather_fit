67df491ed67999489ae3748ce74f23affdd5a234
Trung Nguyen <tnguyen9@macalester.edu>
2017-03-02 18:56:38 -0600

    Create a page for saving clothing item

---
+++ src/pages/save-item/save-item.ts
@@ -0,0 +1,29 @@
+import { Component } from '@angular/core';
+import { NavController, NavParams } from 'ionic-angular';
+import {FormBuilder, FormGroup} from "@angular/forms";
+import {ClothingDataService} from "../../providers/clothing-data-service";
+
+/*
+  Generated class for the SaveItem page.
+
+  See http://ionicframework.com/docs/v2/components/#navigation for more info on
+  Ionic pages and navigation.
+*/
+@Component({
+  selector: 'page-save-item',
+  templateUrl: 'save-item.html'
+})
+export class SaveItemPage {
+
+  clothingItemForm: FormGroup;
+
+  submitAttempt: boolean = false;
+
+  public items = [];
+
+  constructor(public navCtrl: NavController,
+              public navParams: NavParams,
+              public formBuilder: FormBuilder,
+              public storage: ClothingDataService) {}
+
+}
