332915ec6c16a0a17ab6ccae066f80aaf234ca4c
Ayoub <abelemli@macalester.edu>
2017-04-12 22:07:13 -0500

    Implemented user grade update

--- src/pages/home/home.ts
+++ src/pages/home/home.ts
@@ -13,6 +13,8 @@ import {SettingsService} from "../../providers/settings-service";
 
 import {ClothingService} from "../../providers/clothing-service";
 
+import {ClothingItem} from "../../providers/clothing-item";
+
 import {ClothingDataService} from "../../providers/clothing-data-service";
 
 import {Storage} from "@ionic/storage";
@@ -38,7 +40,8 @@ export class HomePage {
   option: any;
 
   recommendation: any;
-
+  clothing_data: any;
+  
   constructor(public navCtrl: NavController,
               private weatherService: WeatherService,
               public geolocationService: GeolocationService,
@@ -53,6 +56,7 @@ export class HomePage {
     this.loadCurrentLocation()
       .then(() => {
         this.loadWeather();
+        this.loadClothing();
         this.loadRecommendation();
       });
     // clothingData.getData()
@@ -64,6 +68,14 @@ export class HomePage {
       .then((pos) => this.weatherService.pos = pos);
   }
 
+  loadClothing() {
+    return this.clothingDataService.getData()
+    .then( (data) => {
+      this.clothing_data = data;
+      }
+    )
+    .catch((error) => console.log("Failed to load clothingDataService\n" + error.toString()))
+  }
   loadRecommendation() {
     this.clothingService.weatherService = this.weatherService;
     return this.clothingService.recommend()
@@ -141,17 +153,39 @@ export class HomePage {
   }
   
   allTapped() {
+    function findIndex (element: ClothingItem, array: Array<ClothingItem>): any {
+      for (let i in array) { if (array[i].url == element.url) { return i } }
+      return -1
+    }
+    
+    function updateItemGrade(item: ClothingItem, array: Array<ClothingItem>) {
+      let new_item_grade = item.grade = ((1-item.grade)*(0.05)) + item.grade
+      let updated_array = array
+      updated_array[findIndex(item,array)].grade = new_item_grade
+      return updated_array
+    }
+    
+    
     if (this.picked.every(Boolean)) {
       //TODO: increase the user grade of all items picked by (1-grade)*10%
-      // console.log(this.recommendation)
-      // console.log(this.storage.get("ClothingData"))
-      let data =  Promise.all([this.clothingDataService.getData()])
-        .then( (values) => {
-          let clothing_items = values[0];
-          //TODO: replace "neutral" with user_gender that we get from settings
-          console.log(clothing_items)
-          return clothing_items
-        })
+      let clothing_types = ["top","bottom","accessories"]
+      let updated_clothing_data = this.clothing_data
+      for (let i in clothing_types) {
+        let chosen_clothing_array = this.recommendation[clothing_types[i]]
+        let item = chosen_clothing_array[this.convertIndexToSlide(i).getActiveIndex()]
+        updated_clothing_data[clothing_types[i]] = updateItemGrade(item, updated_clothing_data[clothing_types[i]])
+      }
+      console.log("Updating---------------------")
+      console.log(updated_clothing_data)
+      this.storage.set("ClothingData", updated_clothing_data)
+        .catch((error) => "Failed to store data");
+      //this.clothingDataService.save(updated_clothing_data)
+      console.log("-----------------------------")
+    }
+    else {
+      console.log("Displaying existing data")
+      console.log(this.clothing_data)
+      console.log("-----------------------------")
     }
     
   }
