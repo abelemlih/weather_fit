cd2448b66c735cedf5ad344e80319415199e2c6b
khintk <kkyaw@macalester.edu>
2017-04-17 19:38:36 -0500

    reinitialize the database every time

--- src/pages/home/home.ts
+++ src/pages/home/home.ts
@@ -125,16 +125,16 @@ export class HomePage {
   ionViewDidLoad() {
     // This will make it repopulate the database every login, which is good for testing purposes.
     // Correctly it should be inside the then() method so that it only runs once.
-    
-    // this.clothingService.initializeDB();
-    this.storage.get('first-login')
-      .then(done => {
-        if (!done) {
-          this.storage.set('first-login', true)
-            .catch((error) => console.log("Can not set first login\n" + error.toString()));
-          this.clothingService.initializeDB();
-        }
-      })
+
+    this.clothingService.initializeDB();
+    // this.storage.get('first-login')
+    //   .then(done => {
+    //     if (!done) {
+    //       this.storage.set('first-login', true)
+    //         .catch((error) => console.log("Can not set first login\n" + error.toString()));
+    //       this.clothingService.initializeDB();
+    //     }
+    //   })
 
   }
 
